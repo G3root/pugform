69d77d38aa0574f02f26d87e6ae1cd79b244b6a8
nafees nazik <nafeesnazik21@gmail.com>
2024-10-28 06:54:27 +0530

    feat: use aria-components

--- app/components/ui/floating-button.tsx
+++ app/components/ui/floating-button.tsx
@@ -1,12 +1,8 @@
 import { PlusIcon } from '@radix-ui/react-icons'
-import { forwardRef } from 'react'
 import { cn } from '~/lib/utils'
 import { Button, type ButtonProps } from './button'
 
-export const FloatingButton = forwardRef<
-	HTMLButtonElement,
-	Omit<ButtonProps, 'children'> & { 'aria-label': string }
->(({ className, ...props }, ref) => {
+export const FloatingButton = ({ className, ...rest }: ButtonProps) => {
 	return (
 		<Button
 			variant="default"
@@ -15,10 +11,9 @@ export const FloatingButton = forwardRef<
 				'fixed bottom-4 left-4 rounded-full w-14 h-14 shadow-lg hover:shadow-xl transition-all duration-300 hover:scale-105',
 				className,
 			)}
-			ref={ref}
-			{...props}
+			{...rest}
 		>
 			<PlusIcon className="h-6 w-6" />
 		</Button>
 	)
-})
+}
--- app/modules/builder/components/form-elements-drawer.tsx
+++ app/modules/builder/components/form-elements-drawer.tsx
@@ -1,13 +1,5 @@
 import { Button } from '~/components/ui/button'
 import { FloatingButton } from '~/components/ui/floating-button'
-import {
-	Sheet,
-	SheetContent,
-	SheetDescription,
-	SheetHeader,
-	SheetTitle,
-	SheetTrigger,
-} from '~/components/ui/sheet'
 
 import {
 	AlignLeft,
@@ -20,6 +12,13 @@ import {
 	Type,
 } from 'lucide-react'
 
+import {
+	DialogContent,
+	DialogHeader,
+	DialogOverlay,
+	DialogTitle,
+	DialogTrigger,
+} from '~/components/ui/dialog'
 import { FIELD_LIST } from '../constants'
 
 const IconMap: Partial<Record<FieldTypes, LucideIcon>> = {
@@ -34,31 +33,34 @@ const IconMap: Partial<Record<FieldTypes, LucideIcon>> = {
 
 export function FormElementsDrawer() {
 	return (
-		<Sheet>
-			<SheetTrigger asChild>
-				<FloatingButton aria-label="add items" />
-			</SheetTrigger>
-			<SheetContent>
-				<SheetHeader>
-					<SheetTitle>Form Elements</SheetTitle>
-					<SheetDescription className="sr-only">Form Elements</SheetDescription>
-				</SheetHeader>
-				<div className="grid gap-4 py-4">
-					{FIELD_LIST.map((item) => {
-						const ItemIcon = IconMap[item.type] as LucideIcon
-						return (
-							<Button
-								className="justify-start"
-								variant="outline"
-								type="button"
-								key={item.label}
-							>
-								<ItemIcon className="w-4 h-4" /> {item.label}
-							</Button>
-						)
-					})}
-				</div>
-			</SheetContent>
-		</Sheet>
+		<DialogTrigger>
+			<FloatingButton aria-label="add items" />
+			<DialogOverlay>
+				<DialogContent side="right" className="sm:max-w-[425px]">
+					{() => (
+						<>
+							<DialogHeader>
+								<DialogTitle>Form Elements</DialogTitle>
+							</DialogHeader>
+							<div className="grid gap-4 py-4">
+								{FIELD_LIST.map((item) => {
+									const ItemIcon = IconMap[item.type] as LucideIcon
+									return (
+										<Button
+											className="justify-start"
+											variant="outline"
+											type="button"
+											key={item.label}
+										>
+											<ItemIcon className="w-4 h-4" /> {item.label}
+										</Button>
+									)
+								})}
+							</div>
+						</>
+					)}
+				</DialogContent>
+			</DialogOverlay>
+		</DialogTrigger>
 	)
 }
