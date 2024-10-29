e2687606143d3415e5aa360d30456e280baee0b4
nafees nazik <nafeesnazik21@gmail.com>
2024-10-29 13:51:35 +0530

    feat: use file routes

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import { vitePlugin as remix } from '@remix-run/dev'
 import { remixDevTools } from 'remix-development-tools'
-import { flatRoutes } from 'remix-flat-routes'
 import { defineConfig } from 'vite'
 import { envOnlyMacros } from 'vite-env-only'
 import tsconfigPaths from 'vite-tsconfig-paths'
@@ -25,24 +24,21 @@ export default defineConfig({
 				v3_singleFetch: true,
 				v3_lazyRouteDiscovery: true,
 			},
-			routes: async (defineRoutes) => {
-				return flatRoutes('routes', defineRoutes, {
-					ignoredRouteFiles: [
-						'.*',
-						'**/*.css',
-						'**/*.test.{js,jsx,ts,tsx}',
-						'**/__*.*',
-						// This is for server-side utilities you want to colocate
-						// next to your routes without making an additional
-						// directory. If you need a route that includes "server" or
-						// "client" in the filename, use the escape brackets like:
-						// my-route.[server].tsx
-						'**/*.server.*',
-						'**/*.client.*',
-					],
+			routes(defineRoutes) {
+				return defineRoutes((route) => {
+					route('/', 'routes/_index.tsx', { index: true })
+					route('dashboard/login', 'routes/(application)/(auth)/login.tsx', {
+						index: true,
+					})
+					route(
+						'dashboard',
+						'routes/(application)/(dashboard)/_layout.tsx',
+						() => {},
+					)
 				})
 			},
 		}),
+
 		tsconfigPaths(),
 	],
 })
