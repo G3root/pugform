81d25a64f831ec8525f6df32e3feed28ba637f79
nafees nazik <nafeesnazik21@gmail.com>
2024-11-27 20:42:59 +0530

    fix: import

--- packages/field-renderer/tsup.config.ts
+++ packages/field-renderer/tsup.config.ts
@@ -2,13 +2,12 @@ import { defineConfig } from 'tsup'
 
 export default defineConfig({
 	entry: ['src/index.ts'],
-	format: ['esm', 'cjs'],
+	format: ['esm'],
 	dts: true,
 	splitting: false,
 	sourcemap: true,
 	clean: true,
-	// jsxFactory: 'h',
-	// jsxFragment: 'Fragment',
-
+	jsxFactory: 'h',
+	jsxFragment: 'Fragment',
 	outDir: 'dist/js',
 })
