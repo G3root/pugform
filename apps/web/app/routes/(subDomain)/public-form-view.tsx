d69b7427213d185d2a3b1715e88d288f62120462
nafees nazik <nafeesnazik21@gmail.com>
2024-12-03 07:42:48 +0530

    feat: use widget for field renderer

--- apps/web/app/routes/(subDomain)/public-form-view.tsx
+++ apps/web/app/routes/(subDomain)/public-form-view.tsx
@@ -1,20 +1,11 @@
-import { parseWithZod } from '@conform-to/zod'
-import { type NewAnswer, db } from '@pugform/database'
-import type { ActionFunctionArgs, LoaderFunctionArgs } from 'react-router';
-import { data, useLoaderData } from 'react-router';
-import { z } from 'zod'
-import { CardForm } from '~/modules/form/components/card-form'
-import { ClassicForm } from '~/modules/form/components/classic-form'
-import { FormMachineContext } from '~/modules/form/state-machines/form-machine'
-import { IntegrationRegistry } from '~/modules/integration/registry'
-import { IntegrationService } from '~/modules/integration/services'
-import type { IntegrationFormData } from '~/modules/integration/types'
-
+import { formWidget } from '@pugform/field-renderer'
+import { useEffect } from 'react'
 import { trpcServer } from '~/trpc/server'
-import { newId } from '~/utils/uuid'
+import type { Route } from './+types/public-form-view'
+import '@pugform/field-renderer/style.css'
 
-export async function loader({ request, context, params }: LoaderFunctionArgs) {
-	const formId = params.formId as string
+export async function loader({ request, context, params }: Route.LoaderArgs) {
+	const formId = params.formId
 
 	const { data } = await trpcServer({ request, context }).form.getPublic({
 		formId,
@@ -24,113 +15,31 @@ export async function loader({ request, context, params }: LoaderFunctionArgs) {
 	}
 }
 
-export async function action({ request, context, params }: ActionFunctionArgs) {
-	const formData = await request.formData()
-	const formId = params.formId as string
+export default function PublicFormView({ loaderData }: Route.ComponentProps) {
+	useEffect(() => {
+		const widget = formWidget()
 
-	const submission = parseWithZod(formData, {
-		schema: z.record(z.string()),
-	})
-
-	if (submission.status !== 'success') {
-		return data(
-			{ result: submission.reply(), status: 'failed' as const },
-			{ status: submission.status === 'error' ? 400 : 200 },
-		)
-	}
-
-	const submissionValues = submission.value
-	const form = await db
-		.selectFrom('form')
-		.where('id', '=', formId)
-		.select(['id', 'organizationId', 'title'])
-		.executeTakeFirstOrThrow()
-
-	const response = await db
-		.insertInto('response')
-		.values({
-			formId: form.id,
-			id: newId('response'),
-			organizationId: form.organizationId,
-			createdAt: new Date(),
-			updatedAt: new Date(),
+		widget.render({
+			selector: '[data-island="widget"]',
+			initialProps: {
+				...loaderData.data.form,
+				...loaderData.data.meta,
+				fields: loaderData.data.fields,
+				pages: loaderData.data.pages,
+			},
 		})
-		.returning(['id', 'createdAt'])
-		.executeTakeFirstOrThrow()
 
-	const fields = await db
-		.selectFrom('field')
-		.where('formId', '=', form.id)
-		.select(['id', 'label', 'type', 'order'])
-		.execute()
-
-	const answersToCreate: NewAnswer[] = []
-
-	const responseData: IntegrationFormData = {
-		id: response.id,
-		formId: formId,
-		formName: form.title,
-		timeStamp: response.createdAt.toISOString(),
-		responses: [],
-	}
-
-	for (const field of fields) {
-		const submission = submissionValues?.[field.id]
-		if (submission) {
-			const answerId = newId('answer')
-
-			answersToCreate.push({
-				id: answerId,
-				answer: submission,
-				order: field.order,
-				question: field.label,
-				responseId: response.id,
-				type: field.type,
-				fieldId: field.id,
-				createdAt: new Date(),
-				updatedAt: new Date(),
-			})
-			responseData.responses.push({
-				id: answerId,
-				answer: submission,
-				question: field.label,
-				type: field.type,
-			})
+		return () => {
+			widget.destroy()
 		}
-	}
-
-	await db.insertInto('answer').values(answersToCreate).execute()
-
-	const integrationService = new IntegrationService(new IntegrationRegistry())
-
-	await integrationService.processFormSubmission({
-		formData: responseData,
-		context: {
-			formId: form.id,
-			responseId: response.id,
-			organizationId: form.organizationId,
-		},
-	})
-
-	return data(
-		{ result: submission.reply(), status: 'success' as const },
-		{ status: 200 },
-	)
-}
-
-export type TFormViewLoader = typeof loader
-export type TFormViewAction = typeof action
-
-export default function PublicFormView() {
-	const { data } = useLoaderData<TFormViewLoader>()
+	}, [])
 
 	return (
-		<FormMachineContext.Provider
-			options={{
-				input: { currentPage: 0, totalPage: data.meta.totalPages },
-			}}
-		>
-			{data.form.layout === 'CLASSIC' ? <ClassicForm /> : <CardForm />}
-		</FormMachineContext.Provider>
+		<div className="flex flex-col w-full min-h-screen h-full items-center justify-center">
+			<div
+				className="w-full h-full flex items-center justify-center"
+				data-island="widget"
+			/>
+		</div>
 	)
 }
