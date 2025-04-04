ec0ed91e33a7800b62adca3432f83f1979587908
nafees nazik <nafeesnazik21@gmail.com>
2025-04-05 01:41:37 +0530

    feat: add id

---
+++ apps/dashboard/app/utils/uuid.ts
@@ -0,0 +1,14 @@
+import { createId } from '@paralleldrive/cuid2'
+import { customAlphabet } from 'nanoid'
+
+export const shortId = customAlphabet('0123456789abcdefghijklmnopqrstuvwxyz')
+
+const prefixes = {
+	project: 'prj',
+} as const
+
+export function newId<TPrefix extends keyof typeof prefixes>(prefix: TPrefix) {
+	return `${prefixes[prefix]}_${createId()}` as const
+}
+
+export const newPublicId = () => shortId(12)
